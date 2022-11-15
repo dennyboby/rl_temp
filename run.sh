504c3227c074e88710f0dbbda2c389909d4063c9
Denny Boby <dennyboby123@gmail.com>
2022-11-15 01:30:51 -0500

    updated sbatch

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 #SBATCH -N 1
-#SBATCH -n 2
+#SBATCH -n 4
 #SBATCH --mem 64G
 #SBATCH --gres=gpu:1
+#SBATCH -C A100
 #SBATCH -p short
-#SBATCH -t 24:00:00
+#SBATCH -t 20:00:00
 #SBATCH -o ~/dboby/rl_temp/slurm_outputs/rljob_%j.out
+#SBATCH --error ~/dboby/rl_temp/slurm_outputs/rljob_error_%j.out
 #SBATCH -J den_rl
 
 echo "RL Job running on $(hostname)"
