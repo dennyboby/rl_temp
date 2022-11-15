3493bda34cc6afe39eb50198697fe0a255315027
Denny Boby <dennyboby123@gmail.com>
2022-11-15 00:59:27 -0500

    updated sbatch script

--- run.sh
+++ run.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+
 #SBATCH -N 1
-#SBATCH --job-name=den_rl
-#SBATCH -t 20:00:00
-#SBATCH -C P100
-#SBATCH --mem 80G
+#SBATCH -n 2
+#SBATCH --mem 64G
 #SBATCH --gres=gpu:1
 #SBATCH -p short
+#SBATCH -t 24:00:00
+#SBATCH -o ~/dboby/rl_temp/slurm_outputs/rljob_%j.out
+#SBATCH -J den_rl
 
-module load cuda10.2/toolkit/10.2.89
-module load cudnn/8.1.1.33-11.2/3k5bbs63
+echo "RL Job running on $(hostname)"
 
-source /home/dboby/rl_env/bin/activate
+echo "Loading Python Virtual Environment"
 
-echo "Starting to run the code"
-python main.py --train_dqn
+source ~/dboby/rl_env/bin/activate
+
+echo "Running Python Code"
+
+python3 ~/dboby/rl_temp/main.py --train_dqn
